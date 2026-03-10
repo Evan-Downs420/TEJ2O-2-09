88d26e4b20bdc40d0e58f930914b98e8b1558172
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-10 13:28:13 -0400

    wasd

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: Feb 2026
+ * This program will play rock, paper, scissors.
 */
 
-basic.showString('Hello, World!')
+
